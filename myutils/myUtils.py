6e34f0ff71d31d6397c77e852f333bab54d5c04d
guillermo <guilled52@gmail.com>
2018-11-01 23:53:15 -0400

    adding batch_labeled_loss_customized function to compute loss from customized training sets

--- myutils/myUtils.py
+++ myutils/myUtils.py
@@ -91,7 +91,7 @@ def save_models(nets_, nets_path_, nets_names, score_meters=None, epoch=0, histo
     :param history_score_dict:
     :return:
     """
-    history_score_dict['epoch']=epoch
+    history_score_dict['epoch']=epoch+1
 
     for idx, net_i in enumerate(nets_):
 
