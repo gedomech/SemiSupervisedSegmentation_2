0d1f6ea12bb0aa88ec93f520c55bc655b5e604f2
jizong <jizong@jizongdeiMac.local>
2018-11-27 10:54:06 -0500

    change learning rate

--- pseudolabel/run_train_onemodel.sh
+++ pseudolabel/run_train_onemodel.sh
@@ -9,14 +9,14 @@ CUDA_VISIBLE_DEVICES=1 python train_onemodel.py --save_dir semi_baseline_best --
 
 
 #
-CUDA_VISIBLE_DEVICES=0 python train_onemodel.py --save_dir semi_without_labeleddata_last --run_semi True --semi_train__update_labeled=False --model_path runs/pretrain_lr_0.01/best.pth --semi_train__lr=0.0001 &
+CUDA_VISIBLE_DEVICES=2 python train_onemodel.py --save_dir semi_without_labeleddata_last --run_semi True --semi_train__update_labeled=False --model_path runs/pretrain_lr_0.01/best.pth --semi_train__lr=0.0001 &
 
 # compared with different batch size
-CUDA_VISIBLE_DEVICES=0 python train_onemodel.py --save_dir semi_batch_1_last --run_semi True  --model_path runs/pretrain_lr_0.01/last.pth --batch_size 1 --semi_train__lr=0.0001
+CUDA_VISIBLE_DEVICES=0 python train_onemodel.py --save_dir semi_batch_1_last --run_semi True  --model_path runs/pretrain_lr_0.01/last.pth --batch_size 1 --semi_train__lr=0.0001 &
 
-CUDA_VISIBLE_DEVICES=0 python train_onemodel.py --save_dir semi_batch_8_last --run_semi True  --model_path runs/pretrain_lr_0.01/last.pth --batch_size 8 --semi_train__lr=0.0001
+CUDA_VISIBLE_DEVICES=1 python train_onemodel.py --save_dir semi_batch_8_last --run_semi True  --model_path runs/pretrain_lr_0.01/last.pth --batch_size 8 --semi_train__lr=0.0001 &
 
-CUDA_VISIBLE_DEVICES=0 python train_onemodel.py --save_dir semi_batch_16_last --run_semi True  --model_path runs/pretrain_lr_0.01/last.pth --batch_size 16 --semi_train__lr=0.0001
+CUDA_VISIBLE_DEVICES=2 python train_onemodel.py --save_dir semi_batch_16_last --run_semi True  --model_path runs/pretrain_lr_0.01/last.pth --batch_size 16 --semi_train__lr=0.0001
 
 
 
