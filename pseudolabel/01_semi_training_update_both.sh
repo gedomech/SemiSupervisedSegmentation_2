5c912d5da71e94ca68487784607fef6551b0039f
Guillermo <guilled52@gmail.com>
2018-11-30 12:25:47 -0500

    changing save dir in bash scripts

--- pseudolabel/01_semi_training_update_both.sh
+++ pseudolabel/01_semi_training_update_both.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=0 python train_onemodel.py --save_dir oracle/demo/update_both/use_oracle --run_semi=True --model_path runs/demo_oracle/pretrain_lr_0.04/best.pth --semi_train__lr=0.0001 \
+OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=0 python train_onemodel.py --save_dir demo_oracle/update_both/use_oracle --run_semi=True --model_path runs/demo_oracle/pretrain_lr_0.04/best.pth --semi_train__lr=0.0001 \
  --semi_train__gamma=0.95 --batch_size=4 --semi_train__loss_name=oracle --semi_train__update_labeled=True --semi_train__update_unlabeled=True --semi_train__max_epoch=100 --labeled_percentate=0.04 &
 
-OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=0 python train_onemodel.py --save_dir oracle/demo/update_both/use_ce --run_semi=True  --model_path runs/demo_oracle/pretrain_lr_0.04/best.pth --semi_train__lr=0.0001 \
+OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=0 python train_onemodel.py --save_dir demo_oracle/update_both/use_ce --run_semi=True  --model_path runs/demo_oracle/pretrain_lr_0.04/best.pth --semi_train__lr=0.0001 \
 --semi_train__gamma=0.95 --batch_size=4 --semi_train__loss_name=crossentropy --semi_train__update_labeled=True --semi_train__update_unlabeled=True --semi_train__max_epoch=100 --labeled_percentate=0.04
