d3db94d6ff128cd56c6c1f5187cd416f5248c59b
Guillermo <guilled52@gmail.com>
2018-11-30 11:51:03 -0500

    correcting bash scripts

--- pseudolabel/01_semi_training_update_unlab.sh
+++ pseudolabel/01_semi_training_update_unlab.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=2 python train_onemodel.py --save_dir demo/oracle/update_unlab/use_oracle --run_semi=True --model_path runs/demo_oracle/pretrain_lr_0.04/best.pth --semi_train__lr=0.0001 \
- --semi_train__gamma=0.95 --batch_size=4 --semi_train__loss_name=oracle --semi_train__update_labeled=False --semi_train__update_unlabeled=True --semi_train__max_epoch=100 &
+OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=2 python train_onemodel.py --save_dir oracle/demo/update_unlab/use_oracle --run_semi=True --model_path runs/demo_oracle/pretrain_lr_0.04/best.pth --semi_train__lr=0.0001 \
+ --semi_train__gamma=0.95 --batch_size=4 --semi_train__loss_name=oracle --semi_train__update_labeled=False --semi_train__update_unlabeled=True --semi_train__max_epoch=100 --labeled_percentate=0.04 &
 
-OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=2 python train_onemodel.py --save_dir demo/oracle/update_unlab/use_ce --run_semi=True  --model_path runs/demo_oracle/pretrain_lr_0.04/best.pth --semi_train__lr=0.0001 \
---semi_train__gamma=0.95 --batch_size=4 --semi_train__loss_name=crossentropy --semi_train__update_labeled=False --semi_train__update_unlabeled=True --semi_train__max_epoch=100
+OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=2 python train_onemodel.py --save_dir oracle/demo/update_unlab/use_ce --run_semi=True  --model_path runs/demo_oracle/pretrain_lr_0.04/best.pth --semi_train__lr=0.0001 \
+--semi_train__gamma=0.95 --batch_size=4 --semi_train__loss_name=crossentropy --semi_train__update_labeled=False --semi_train__update_unlabeled=True --semi_train__max_epoch=100 --labeled_percentate=0.04
 git
