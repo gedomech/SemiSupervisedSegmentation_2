1c56ee219989099bf312e0b5416f02729da3bf5c
Guillermo <guilled52@gmail.com>
2018-12-03 18:05:53 -0500

    correcting training from scratch script

--- pseudolabel/semi_training_from_scratch_diff_lr_1e-3.sh
+++ pseudolabel/semi_training_from_scratch_diff_lr_1e-3.sh
@@ -9,7 +9,7 @@ OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=1 python train_onemodel.py --save_dir de
 
 # using crossentropy loss function
 OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=1 python train_onemodel.py --save_dir demo_oracle/semi_from_scratch_diff_lr/lr_0.001/use_ce --run_semi=True \
---semi_train__lr=0.001 --semi_train__gamma=0.95 --batch_size=4 --semi_train__loss_name=crossentropy \
+--load_pretrain=False --semi_train__lr=0.001 --semi_train__gamma=0.95 --batch_size=4 --semi_train__loss_name=crossentropy \
 --semi_train__update_labeled=True --semi_train__update_unlabeled=True --semi_train__max_epoch=100 --labeled_percentate=0.04
 
 
