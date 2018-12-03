108531ec69a8e15f3cb560b493cdfe5017c7a46a
Guillermo <guilled52@gmail.com>
2018-12-03 14:48:22 -0500

    adding batch script for running self-training from scratch using diff lr

--- pseudolabel/semi_training_from_scratch_diff_lr.sh
+++ pseudolabel/semi_training_from_scratch_diff_lr.sh
@@ -7,10 +7,10 @@ OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=0 python train_onemodel.py --save_dir de
 --semi_train__lr=0.0001 --semi_train__gamma=0.95 --batch_size=4 --semi_train__loss_name=oracle \
 --semi_train__update_labeled=True --semi_train__update_unlabeled=True --semi_train__max_epoch=100 --labeled_percentate=0.04 &
 
-# using crossentropy loss function
+ using crossentropy loss function
 OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=0 python train_onemodel.py --save_dir demo_oracle/semi_from_scratch_diff_lr/lr_0.0001//use_ce --run_semi=True \
 --semi_train__lr=0.0001 --semi_train__gamma=0.95 --batch_size=4 --semi_train__loss_name=crossentropy \
---semi_train__update_labeled=True --semi_train__update_unlabeled=True --semi_train__max_epoch=100 --labeled_percentate=0.04 &
+--semi_train__update_labeled=True --semi_train__update_unlabeled=True --semi_train__max_epoch=100 --labeled_percentate=0.04
 
 
 # =================== learning rate 0.001 (1e-3) ===================
@@ -23,17 +23,17 @@ OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=1 python train_onemodel.py --save_dir de
 # using crossentropy loss function
 OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=1 python train_onemodel.py --save_dir demo_oracle/semi_from_scratch_diff_lr/lr_0.001//use_ce --run_semi=True \
 --semi_train__lr=0.001 --semi_train__gamma=0.95 --batch_size=4 --semi_train__loss_name=crossentropy \
---semi_train__update_labeled=True --semi_train__update_unlabeled=True --semi_train__max_epoch=100 --labeled_percentate=0.04 &
+--semi_train__update_labeled=True --semi_train__update_unlabeled=True --semi_train__max_epoch=100 --labeled_percentate=0.04
 
 
 # =================== learning rate 0.01 (1e-2) ===================
 
 # using oracle loss function
-OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=2 python train_onemodel.py --save_dir demo_oracle/semi_from_scratch_diff_lr/lr_0.01/use_oracle --run_semi=True \
+OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=1 python train_onemodel.py --save_dir demo_oracle/semi_from_scratch_diff_lr/lr_0.01/use_oracle --run_semi=True \
 --semi_train__lr=0.01 --semi_train__gamma=0.95 --batch_size=4 --semi_train__loss_name=oracle \
 --semi_train__update_labeled=True --semi_train__update_unlabeled=True --semi_train__max_epoch=100 --labeled_percentate=0.04 &
 
 # using crossentropy loss function
-OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=2 python train_onemodel.py --save_dir demo_oracle/semi_from_scratch_diff_lr/lr_0.01//use_ce --run_semi=True \
+OMP_NUM_THREADS=4  CUDA_VISIBLE_DEVICES=1 python train_onemodel.py --save_dir demo_oracle/semi_from_scratch_diff_lr/lr_0.01//use_ce --run_semi=True \
 --semi_train__lr=0.01 --semi_train__gamma=0.95 --batch_size=4 --semi_train__loss_name=crossentropy \
 --semi_train__update_labeled=True --semi_train__update_unlabeled=True --semi_train__max_epoch=100 --labeled_percentate=0.04
